3bf890b0c4f59d706c9b98e017ba9baff397c5c2
Cody Roberts <cody@honestbuildings.com>
2014-03-26 21:27:14 -0400

    initial commit

---
+++ src/directives/angular-linkedin-login.js
@@ -0,0 +1,152 @@
+angular.module('linkedinExample.directives', [])
+    .directive('linkedinLogin',
+        ['$rootScope', '$interval',
+            function ($rootScope, $interval) {
+                return {
+                    restrict: 'AE',
+                    replace: true,
+                    templateUrl: '/src/directives/angular-linkedin-login-ptl.html',
+                    scope: true,
+                    compile: function (tElem, tAttrs) {
+
+                        var linkedinLibLoaded = false,
+                            linkedinLibInitialized = false;
+            
+                        window.linkedinLibInit = function(){
+                            linkedinLibInitialized = true;
+                        };
+            
+                        if(!linkedinLibLoaded){
+                
+                            linkedinLibLoaded = true;
+                
+                            $.getScript("//platform.linkedin.com/in.js?async=true", function success() {
+                                IN.init({
+                                    onLoad: "linkedinLibInit",
+                                    api_key: "5L-GzSuqtGjn6ZPku-Ws5sEZ9yLn_klDG3WPnAW1sTwTG9eeXyMRcKcUu5QlR_VZ",
+                                    credentials_cookie: true
+                                });
+                            });
+                
+                        }
+
+                        return function (scope, elem, attrs) {
+                            
+                            scope._buttonText = tAttrs.linkedinButtonText || 'Connect';
+                            var _authorizedHandlerName = tAttrs.linkedinAuthorized || null;
+                            var _authorizedHandler = _authorizedHandlerName ? scopescope[_authorizedHandlerName] : null;
+                            var _profileDataHandlerName = tAttrs.linkedinProfileData || null;
+                            var _profileDataHandler = _profileDataHandlerName ? scope[_profileDataHandlerName] : null;
+                            var _successMsg = tAttrs.linkedinSuccessMsg || 'Linkedin Connection Authorized';
+                            var ua = navigator.userAgent || 'unknown';
+                            var _iOS = BrowserUtil.iOS();
+                            var _iOSinterval = null;
+                            
+                            if(!_authorizedHandler && !_profileDataHandler){
+                                throw "You must provide a 'hb-form-authorize' or 'hb-form-profile-data' on the scope.";
+                            }
+                            if(_iOS){
+                                // hide linkedin button for iOS because the LinkedIn JS API does not work, argh...
+                                scope.linkedinMsg = scope.linkedinMsg || {};
+                                scope.linkedinMsg.successMsg = true;
+                                if(_profileDataHandler && typeof _profileDataHandler == 'function'){
+                                    scope[_profileDataHandlerName]({hideLinkedin:true});
+                                }else{
+                                    scope[_authorizedHandlerName]({hideLinkedin:true});
+                                }
+                                return;
+                            }
+                            
+                            scope.linkedinMsg = scope.linkedinMsg || {};
+                            scope.linkedinMsg.loaded = false;
+                            scope.linkedinMsg.showButton = true;
+                            scope.linkedinMsg.message = tAttrs.linkedinMsg || null;
+                            scope.linkedinMsg.loading = false;
+                            scope.linkedinMsg.errorMsg = null;
+                            scope.linkedinMsg.successMsg = null;
+                            
+                            scope.onLinkedinAuthClick = function($ev){
+                                authorizeLinkedin();
+                            };
+                            
+                            var authorizeLinkedin = function(){
+                                scope.linkedinMsg.loading = true;
+                                if(IN.User.isAuthorized()){
+                                    linkedinAuthorized();
+                                }else{
+                                    IN.User.authorize(linkedinAuthorized);
+                                    /*
+                                    if(_iOS){
+                                        _iOSinterval = $interval(function(count){
+                                            var linkedinCookieFound = false;
+                                            var cookies = document.cookie.split(';');
+                                            for(var i=0;i<cookies.length;i++){
+                                                if(cookies[i].indexOf('linkedin_oauth') !== -1){
+                                                    var cookieParts = cookies[i].split('=');
+                                                    if(cookieParts[1] && _.isEmpty(cookieParts[1])){
+                                                        linkedinCookieFound = true;
+                                                        break;
+                                                    }
+                                                }
+                                            }
+                                            if(linkedinCookieFound || count >= 9){
+                                                $interval.cancel(_iOSinterval);
+                                                linkedinAuthorized();
+                                            }
+                                        }, 200, 10);
+                                    }
+                                    */
+                                }
+                            };
+
+                            var linkedinAuthorized = function(){
+                                if(_profileDataHandler && typeof _profileDataHandler == 'function'){
+                                    IN.API.Profile("me")
+                                        .fields('id','first-name','last-name','location','industry','headline','picture-urls::(original)','email-address')
+                                        .result(function(data){
+                                            linkedinDataTransform(data);
+                                        })
+                                        .error(function(err){
+                                            scope.linkedinMsg.loading = false
+                                            scope.linkedinMsg.errorMsg = 'Unable to get LinkedIn profile information.  Please re-authorize.';
+
+                                        });
+                                }else{
+                                    scope.linkedinMsg.loading = false;
+                                    scope.linkedinMsg.successMsg = _successMsg;
+                                }
+                                if(_authorizedHandlerName && typeof scope[_authorizedHandlerName] == 'function')
+                                    scope.$apply(scope[_authorizedHandlerName]({auth:true}));
+                            };
+      
+                            var linkedinDataTransform = function(linData){
+                                var linUser = ( (linData && linData.values) ? linData.values[0] : null );
+                                if(linUser){
+                                    scope.linkedinMsg.showButton = false;
+                                    scope.linkedinMsg.successMsg = _successMsg;
+                                    var data = {
+                                        user_email: linUser.emailAddress,
+                                        first_name: linUser.firstName,
+                                        last_name: linUser.lastName,
+                                        industry: linUser.industry,
+                                        headline: linUser.headline,
+                                        linkedin_id: linUser.id,
+                                        location: (linUser.location ? linUser.location.name : ''),
+                                        country_code: (linUser.location ? linUser.location.country.code : ''),
+                                        headline: linUser.headline,
+                                        thumb_url: ((linUser.pictureUrls && linUser.pictureUrls._total > 0) ? linUser.pictureUrls.values[0] : '')
+                                    };
+                                    if(_profileDataHandlerName && typeof scope[_profileDataHandlerName] == 'function'){
+                                        scope.$apply(scope[_profileDataHandlerName](data));
+                                    }
+                                }else{
+                                    scope.linkedinMsg.loading = false;
+                                    scope.linkedinMsg.successMsg = 'Unable to get LinkedIn profile information.  Please re-authorize.';
+                                }
+                            };
+                
+                        }
+                    }
+                }
+            }
+        ]);
---
+++ test/hb-linkedin-spec.js
@@ -0,0 +1,202 @@
+describe("linkedin-login", function () {
+    var $rootScope,
+        $compile,
+        $scope,
+        el,
+        getScriptSpy,
+        INinitSpy,
+        INUserIsAuthorizedSpy,
+        INUserAuthorizeSpy,
+        INAPIProfileSpy,
+        $body = $('body'),
+        fullHtml = '<div hb-linkedin hb-linkedin-button-text="Authorize" hb-linkedin-success-msg="Success!" hb-linkedin-authorized="onAuthorized" hb-linkedin-profile-data="onProfileData"></div>',
+        authOnlyHtml = '<div hb-linkedin hb-linkedin-button-text="Authorize" hb-linkedin-success-msg="Success!" hb-linkedin-authorized="onAuthorized"></div>',
+        linkedinProfileData = {
+          "_total": 1,
+          "values": [{
+            "_key": "~",
+            "emailAddress": "james@jamesroberts.name",
+            "firstName": "James",
+            "headline": "Lead Developer at Honest Buildings",
+            "id": "1234",
+            "industry": "Marketing and Advertising",
+            "lastName": "Roberts",
+            "location": {
+              "country": {"code": "us"},
+              "name": "Greater Seattle Area"
+            },
+            "pictureUrls": {
+              "_total": 1,
+              "values": ["http://m.c.lnkd.licdn.com/mpr/mprx/0_1p_n01148nEr8WTGldCZyt-V8bnrbMSGlfjRJ-3qDtcP8Z0u12GsU4iYx0V"]
+            }
+          }]
+        },
+        transformedUserData = {
+            "user_email":"james@jamesroberts.name",
+            "first_name":"James",
+            "last_name":"Roberts",
+            "industry":"Marketing and Advertising",
+            "headline":"Lead Developer at Honest Buildings",
+            "linkedin_id":"1234",
+            "location":"Greater Seattle Area",
+            "country_code":"us",
+            "thumb_url":"http://m.c.lnkd.licdn.com/mpr/mprx/0_1p_n01148nEr8WTGldCZyt-V8bnrbMSGlfjRJ-3qDtcP8Z0u12GsU4iYx0V"
+        };
+
+    window.IN = {
+        init: function(params){
+            window[params.onLoad]([]);
+        },
+        User: {
+            isAuthorized: function(){
+                return false;
+            },
+            authorize: function(authCallback){
+                authCallback(true);
+            }
+        },
+        API: {
+            Profile: function(name){
+                return {
+                    fields: function(f){
+                        return {
+                            result: function(resCallback){
+                                resCallback(linkedinProfileData);
+                                return {
+                                    error: function(errCallback){
+                                        errCallback({message: 'some error'});
+                                    }
+                                }
+                            }
+                        }
+                    }
+                };
+            }
+        }
+    };
+    
+    beforeEach(function () {
+
+        //Instantiate module with mocks
+        angular.mock.module('hb.core', 'templates');
+
+        inject(function ($injector, _$compile_) {
+            $rootScope = $injector.get('$rootScope');
+            $compile = _$compile_;
+            $scope = $rootScope.$new();
+            $scope.linkedinMsg = {successMsg:null};
+            $scope.onAuthorized = jasmine.createSpy("onAuthorized");
+            $scope.onProfileData = jasmine.createSpy("onProfileData");
+            getScriptSpy = spyOn($, 'getScript').andCallFake(function(url, s){
+                s();
+            });
+            INinitSpy = spyOn(window.IN, 'init').andCallThrough();
+            INUserIsAuthorizedSpy = spyOn(IN.User, 'isAuthorized').andCallThrough();
+            INUserAuthorizeSpy = spyOn(IN.User, 'authorize').andCallThrough();
+            INAPIProfileSpy = spyOn(IN.API, 'Profile').andCallThrough();
+            el = $compile(angular.element(fullHtml))($scope);
+        });
+
+        $body.append(el);
+        $rootScope.$digest();
+    });
+
+    afterEach(function () {
+        $body.empty();
+    });
+
+    describe("compile time", function () {
+        it("Should throw an error if the element is compiled without an 'hb-form-authorize' or 'hb-form-profile-data' attribute", function () {
+            expect(function () {
+                $compile(angular.element('<div hb-linkedin></div>'))($scope);
+                $scope.$digest();
+            }).toThrow();
+        });
+        
+        it("Should initialize LinkedIn 'IN' on the window object and set linkedinLibInitialized to true",function(){
+            expect(getScriptSpy).toHaveBeenCalled();
+            expect(INinitSpy).toHaveBeenCalledWith({ onLoad : 'linkedinLibInit', api_key : 'myLinkedinKey', credentials_cookie : true });
+            expect(window.linkedinLibInit).toBeTruthy();
+        });
+        
+        
+    });
+
+    describe("link time", function () {
+        var myScope;
+        beforeEach(function () {
+            myScope = $scope.$$childTail;
+        });
+        
+        it("Should go through authorization and return profile data when scope.onLinkedinAuthClick() is called",function(){
+            myScope.onLinkedinAuthClick();
+            expect(INUserIsAuthorizedSpy).toHaveBeenCalled();
+            expect(INUserAuthorizeSpy).toHaveBeenCalled();
+            expect(INAPIProfileSpy).toHaveBeenCalled();
+            expect($scope.onProfileData).toHaveBeenCalledWith(transformedUserData);
+        });
+        
+        it("Should only call onAuthorized when onProfileData attribute is not set", function(){
+            el = $compile(angular.element(authOnlyHtml))($scope);
+            $scope.$digest();
+            myScope = $scope.$$childTail;
+            myScope.onLinkedinAuthClick();
+            expect(INUserIsAuthorizedSpy).toHaveBeenCalled();
+            expect(INUserAuthorizeSpy).toHaveBeenCalled();
+            expect(INAPIProfileSpy).not.toHaveBeenCalled();
+            expect($scope.onAuthorized).toHaveBeenCalledWith({auth:true});
+        });
+        
+        it("Should call authorized directly and return profile data when already authenticated.",function(){
+            
+            IN.User.isAuthorized = function(){ return true; };
+            INUserIsAuthorizedSpy = spyOn(IN.User, 'isAuthorized').andCallThrough();
+            el = $compile(angular.element(authOnlyHtml))($scope);
+            $scope.$digest();
+            myScope = $scope.$$childTail;
+            myScope.onLinkedinAuthClick();
+            expect(INUserIsAuthorizedSpy).toHaveBeenCalled();
+            expect(INUserAuthorizeSpy).not.toHaveBeenCalled();
+            expect(INAPIProfileSpy).not.toHaveBeenCalled();
+            expect($scope.onAuthorized).toHaveBeenCalledWith({auth:true});
+            
+        });
+        
+        it("Should set scope.linkedInErrorMsg when trying to transform bad data from LinkedIn.",function(){
+            
+            IN.User.isAuthorized = function(){ return true; };
+            INUserIsAuthorizedSpy = spyOn(IN.User, 'isAuthorized').andCallThrough();
+            linkedinProfileData = null;
+            el = $compile(angular.element(fullHtml))($scope);
+            $scope.$digest();
+            myScope = $scope.$$childTail;
+            myScope.onLinkedinAuthClick();
+            expect(INUserIsAuthorizedSpy).toHaveBeenCalled();
+            expect(INAPIProfileSpy).toHaveBeenCalled();
+            expect(myScope.linkedinMsg.errorMsg).toEqual('Unable to get LinkedIn profile information.  Please re-authorize.');
+            
+        });
+        
+        
+    });
+
+    describe("iOS", function () {
+        it("Should set linkedinMsg.successMsg to true, call onAuthorized and return if userAgent is iOS", function(){
+            BrowserUtil.iOS = function(){return true;};
+            el = $compile(angular.element(authOnlyHtml))($scope);
+            $scope.$digest();
+            expect($scope.linkedinMsg.successMsg).toBeTruthy();
+            expect($scope.onAuthorized).toHaveBeenCalledWith({ hideLinkedin : true });
+        });
+        it("Should set linkedinMsg.successMsg to true, call onProfileData and return if userAgent is iOS", function(){
+            var profileDataOnlyHtml = '<div hb-linkedin hb-linkedin-button-text="Authorize" hb-linkedin-success-msg="Success!" hb-linkedin-profile-data="onProfileData"></div>';
+            BrowserUtil.iOS = function(){return true;};
+            el = $compile(angular.element(profileDataOnlyHtml))($scope);
+            $scope.$digest();
+            expect($scope.linkedinMsg.successMsg).toBeTruthy();
+            expect($scope.onProfileData).toHaveBeenCalledWith({ hideLinkedin : true });
+        });
+        
+    });
+    
+});
